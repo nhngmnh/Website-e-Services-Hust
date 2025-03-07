6dc89908aa11f03a9cc88163c70def137d05b2a7
Minh <nhungocminh2004@gmail.com>
2025-03-08 00:29:22 +0700

    folder 8thang3

---
+++ public/thiep83/config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Người uể oải nhất';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Tặng mi cùng tất cả các em hoa hậu BF2 K69 😏';
+const contentCard = 'Dù 8/3 là ngày phụ nữ Việt Nam chứ không phải ngày thiếu nữ Việt Nam nhưng mi từng bảo tôi là một lời khen chẳng mất tiền mua, lựa lời mồm mép cho vừa lòng nhau. Không ấy chúc mi có một ngày thật tràn đầy năng lượng, xinh đẹp và ăn nhiều lên cho tăng cân nhé :v ❤️';
+
+const giftImage = 'giang.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
