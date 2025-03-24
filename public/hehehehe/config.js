4d1ac5a265f5cf34483eac9519f8c388b471e48b
Minh <nhungocminh2004@gmail.com>
2025-03-24 23:43:53 +0700

    change some folder

--- public/hehehehe/config.js
+++ public/hehehehe/config.js
@@ -1,5 +1,5 @@
 const nameGirl = 'Người uể oải nhất';
-const giftUrl = 'http://nodemy.vn';
+const giftUrl = '';
 const eventName = 'Chúc Mừng 8-3';
 const titleCard = 'Tặng mi cùng tất cả các em hoa hậu BF2 K69 😏';
 const contentCard = 'Dù 8/3 là ngày phụ nữ Việt Nam chứ không phải ngày thiếu nữ Việt Nam nhưng mi từng bảo tôi là một lời khen chẳng mất tiền mua, lựa lời mồm mép cho vừa lòng nhau. Không ấy chúc mi có một ngày thật tràn đầy năng lượng, xinh đẹp và ăn nhiều lên cho tăng cân nhé :v ❤️';
